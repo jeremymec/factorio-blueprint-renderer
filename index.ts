1bc631bfa4ee40bcb39dc640f75fd8d438119b7e
Jeremy Mechen <jeremymechen1997@gmail.com>
2021-05-04 01:46:37 +1200

    Initial commit; add basic parsing of blueprint string

---
+++ index.ts
@@ -0,0 +1,131 @@
+interface Entity {
+    type: String
+    position: [number, number]
+}
+
+interface Blueprint {
+    blueprint: {
+        icons:
+            {
+                signal: {
+                    type: string,
+                    name: string
+                },
+                index: number
+            }[],
+        entities: 
+        {
+            entity_number: number,
+            name: string,
+            position: {
+                x: number,
+                y: number
+            }
+        }[]
+        item: string,
+        version: number
+    }
+}
+
+function render(blueprintJSON: String) {
+    // convertBlueprintStringToBlueprint
+    // renderBlueprint
+    // return blueprint
+}
+
+function convertBlueprintJSONToBlueprint(blueprint: Blueprint) {
+
+    // let blueprint: Blueprint = JSON.parse(blueprintString)
+    const xOffset = blueprint.blueprint.entities.reduce((acc, value) => {
+        return Math.abs(value.position.x) < acc.position.x ? value : acc
+    }).position.x
+
+    const yOffset = blueprint.blueprint.entities.reduce((acc, value) => {
+        return Math.abs(value.position.y) < acc.position.y ? value : acc
+    }).position.y
+
+    let entities: Entity[] = blueprint.blueprint.entities.map((entity => {
+        return {
+            type: entity.name,
+            position: [entity.position.x - xOffset, entity.position.y - yOffset]
+        }
+    }))
+
+    return entities
+}
+
+let blueprintJSON = {
+    "blueprint": {
+        "icons": [
+            {
+                "signal": {
+                    "type": "item",
+                    "name": "rail"
+                },
+                "index": 1
+            }
+        ],
+        "entities": [
+            {
+                "entity_number": 1,
+                "name": "straight-rail",
+                "position": {
+                    "x": 149,
+                    "y": 15
+                }
+            },
+            {
+                "entity_number": 2,
+                "name": "straight-rail",
+                "position": {
+                    "x": 151,
+                    "y": 15
+                }
+            },
+            {
+                "entity_number": 3,
+                "name": "straight-rail",
+                "position": {
+                    "x": 153,
+                    "y": 15
+                }
+            },
+            {
+                "entity_number": 4,
+                "name": "straight-rail",
+                "position": {
+                    "x": 155,
+                    "y": 15
+                }
+            },
+            {
+                "entity_number": 5,
+                "name": "straight-rail",
+                "position": {
+                    "x": 157,
+                    "y": 17
+                }
+            },
+            {
+                "entity_number": 6,
+                "name": "straight-rail",
+                "position": {
+                    "x": 159,
+                    "y": 17
+                }
+            },
+            {
+                "entity_number": 7,
+                "name": "straight-rail",
+                "position": {
+                    "x": 161,
+                    "y": 17
+                }
+            }
+        ],
+        "item": "blueprint",
+        "version": 281479273775104
+    }
+}
+
+console.log(convertBlueprintJSONToBlueprint(blueprintJSON))
