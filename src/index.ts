1e6d6dab5bdb3fcd26df246abddbac464eda0b9f
Jeremy Mechen <jeremymechen1997@gmail.com>
2021-06-08 23:31:48 +1200

    Use hooks and state to render

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import MyComponent from './renderedBlueprint'
+import RenderedBlueprint from './renderedBlueprint'
 // import { Entity, Blueprint, convertBlueprintToEntitiesList } from './converter'
 
-export { MyComponent }
+export { RenderedBlueprint }
--- src/renderedBlueprint.tsx
+++ src/renderedBlueprint.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GridLayout from 'react-grid-layout';
 import { RenderedSquare } from './renderedSquare'
-import { Blueprint, convertBlueprintToFactorioGrid } from './converter'
+import { Blueprint, convertBlueprintToFactorioGrid, FactorioGrid } from './converter'
 import grass from './assets/grass.png'
 
 interface RenderedBlueprintProps {
-  blueprint: Blueprint;
+  blueprintData: Blueprint,
+  isLoading: boolean
 }
  
-const RenderedBlueprint = ({ blueprint }: RenderedBlueprintProps) => {
-  let grid = convertBlueprintToFactorioGrid(blueprint)
+const RenderedBlueprint = ({ blueprintData, isLoading }: RenderedBlueprintProps) => {
+
+  const [grid, setGrid] = useState<FactorioGrid>()
+
+  useEffect(() => {
+    console.log("Effect used")
+    if (!isLoading) {
+      let factorioGrid = convertBlueprintToFactorioGrid(blueprintData)
+      console.log(factorioGrid)
+      setGrid(factorioGrid)
+    }
+  }, [blueprintData, isLoading])
 
   const gridStyle = {
     backgroundImage: `url('${grass}')`,
@@ -17,6 +28,14 @@ const RenderedBlueprint = ({ blueprint }: RenderedBlueprintProps) => {
   }
 
   return (
+    isLoading
+    ?
+    <div><h1>Loading</h1></div>
+    :
+    grid == undefined
+    ?
+    <div><h1>Processing</h1></div>
+    :
     <GridLayout className="layout" cols={grid.width} rowHeight={64} width={200} style={gridStyle} autoSize={true} margin={[0, 0]} compactType={null}>
       {grid.entities.map((entity, index) => {
         return <div key={index} data-grid={{x: entity.position[0], y: entity.position[1], w: entity.size[0], h: entity.size[1]}}>
